8c3b7bc16272e1be96b1ec3acb9bdebcd13c550f
BennyHallett <benny.hallett@gmail.com>
2014-02-26 08:34:58 +1100

    Added collision component to player

--- test/factory/player_test.rb
+++ test/factory/player_test.rb
@@ -6,17 +6,25 @@ require 'conquest/factory/player'
 class PlayerFactoryTest < Minitest::Test
 
   def setup
+    @world = mock('object')
     @factory = PlayerFactory.new
+    @player = @factory.create @world
   end
 
   def test_new_player_has_position_component
-    player = @factory.create
-    assert player.has? :position
+    assert @player.has? :position
   end
 
   def test_new_player_has_movement_component
-    player = @factory.create
-    assert player.has? :movement
+    assert @player.has? :movement
+  end
+
+  def test_new_player_has_symbol_component
+    assert @player.has? :symbol
+  end
+
+  def test_new_player_has_collision_component
+    assert @player.has? :collision
   end
 
 end
